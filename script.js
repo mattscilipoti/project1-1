53d8861c026a5c7ccd8edcf10379992379261d5c
Jorge <jlpimentel@gmail.com>
2015-10-28 19:54:14 -0400

    third commit: shuffle button working properly. Still need to work on the rest of the functionality

--- script.js
+++ script.js
@@ -1,50 +1,61 @@
-// document.getElementById("score").addEventListener("click", function(){
-// });
-// document.getElementById("wrong-answer").addEventListener("click", function(){
-// });
-// document.getElementById("score").addEventListener("click", function(){
-// });
-//
-// document.getElementById("score").addEventListener("click", function(){
+// window.onload = function () {
+//   console.log("It's loaded!");
+// //   document.getElementById("reset").addEventListener("click", startGame);
+//   console.log("listening");
+//};
 
-// });
-
-document.getElementById("reset").addEventListener("click", function(){
-  console.log("listening");
-  var order = [];
-  var count = 0;
-  var mySet = new Set();
-  function returnRandomNumber(){
-      var cards = 6;
-      var randomnumber = Math.random();
-      var randomlyselectedcard = Math.floor(randomnumber*cards);
-      return randomlyselectedcard;
-   }
-   while(count < 6){
-       var a = returnRandomNumber();
-       if(!mySet.has(a)){
-           mySet.add(a);
-           order.push(a);
-           count++ ;
-       }
-   }
-   console.log(order);
-  var qaPairs = [
-    [["What is JavaScript?"],["answer"]],
-    [["What is the DOM?"],["answer"]],
-        [["Give 3 examples of AI"],["answer"]],
-            [["What is computer programming?"],["answer"]],
-                [[""],["answer"]],
-                    [["blabla?"],["answer"]]
-  ];
-
-  console.log(qaPairs[0][0]); //question - front
-  console.log(qaPairs[0][1]); //answer - back
-  var answerString = (qaPairs[0][1]).join("");
-  console.log(answerString);
-
-
-
-
-
+$(document).ready(function(){
 });
+
+function returnRandomSelected(){
+    var order = [];
+    var count = 0;
+    var mySet = new Set();
+    function randomSelected(){
+     var cards = 6;
+     var randomnumber = Math.random();
+     var randomlyselectedcard = Math.floor(randomnumber*cards);
+     return randomlyselectedcard;
+    }
+    while(count < 6){
+      var a = randomSelected();
+      if(!mySet.has(a)){
+          mySet.add(a);
+          order.push(a);
+          count++ ;
+      }
+    }
+    return order;
+  }
+    var cOrder = returnRandomSelected();
+  /***********************/
+var cardList = [
+  ["#one .front","#one .back"],
+  ["#two .front","#two .back"],
+  ["#three .front","#three .back"],
+  ["#four .front","#four .back"],
+  ["#five .front","#five .back"],
+  ["#six .front","#six .back"],
+];
+var qaPairs = [
+  ["Capital of Brazil","Brazilia"],
+  ["Capital of China","Beijing"],
+  ["Capital of the U.S.","Washington D.C"],
+  ["Capital of Venezuela","Caracas"],
+  ["Capital of Cuba","Havana"],
+  ["Capital of Chile","Santiago"]
+];
+function startGame(){
+  for(var x = 0; x < 6; x++){
+  $(cardList[x][0]).text(qaPairs[cOrder[x]][0]);
+  $(cardList[x][1]).text(qaPairs[cOrder[x]][1]);
+  }
+}
+startGame();
+function listeners(){
+  $("#reset").on("click", function(){
+    cOrder = returnRandomSelected();
+    startGame();
+  });
+}
+listeners();
